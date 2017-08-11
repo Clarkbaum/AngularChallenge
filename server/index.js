61ebf044de1e23b877cbecba797c0c4e1ab7a645
Nathan Clark Baumgartner <nbaumg1193@gmail.com>
2017-08-11 14:07:55 -0500

    server index.js

--- server/index.js
+++ server/index.js
@@ -1,1 +1,22 @@
 //index.js
+const bodyParser = require('body-parser');
+const config = require('./config/config.js');
+const article = require('./controllers/articles.js');
+
+require('./config/database.js')(config);
+
+
+const app = express();
+module.exports = app;
+
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+  res.setHeader('access-control-allow-origin', '*');
+  res.setHeader('access-control-allow-methods', 'POST, PUT, GET, DELETE');
+  res.setHeader('access-control-allow-headers', 'x-parse-application-id, x-parse-rest-api-key, Content-Type, Accept');
+
+  //res.setHeader('Content-Type', 'application/json');
+  next();
+});

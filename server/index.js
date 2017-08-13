cb3cdb604fd980d817ae05a9d1e07f7157245c3d
Nathan Clark Baumgartner <nbaumg1193@gmail.com>
2017-08-12 23:17:49 -0500

    post and put

--- server/index.js
+++ server/index.js
@@ -20,3 +20,12 @@ app.use(function(req, res, next) {
   //res.setHeader('Content-Type', 'application/json');
   next();
 });
+
+app.post('/articles', function(req, res){
+  article.create(req, res)
+})
+
+app.put('/articles/:article', function(req, res){
+  //the put will include the article id in the request.params
+  article.update(req, res)
+})

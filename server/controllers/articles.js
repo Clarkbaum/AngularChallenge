b15b1304ad7a0265c7f7fe47f29a5afc5e8580f6
Nathan Clark Baumgartner <nbaumg1193@gmail.com>
2017-08-23 11:21:40 -0500

    controller

--- server/controllers/articles.js
+++ server/controllers/articles.js
@@ -1,1 +1,9 @@
 //articles.js
+const Article = require('../models/articles.js');
+
+exports.create = (request, response) => {
+  const article = Object.assign({}, request.body);
+  Article.create(article)
+  .then(result => response.status(201).json(result))
+  .catch((err) => console.log("article.create error", err));
+};

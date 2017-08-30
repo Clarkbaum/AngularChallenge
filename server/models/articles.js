d46bbcaa22f79184b34146d97e99459f420c0739
Nathan Clark Baumgartner <nbaumg1193@gmail.com>
2017-08-30 14:18:41 -0500

    modele schema

--- server/models/articles.js
+++ server/models/articles.js
@@ -1,1 +1,14 @@
 //articles.js
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const articlesSchema = Schema({
+  title: String,
+  author: String,
+  date: String,
+  articalBody: String
+}, { strict: 'throw' });
+
+
+module.exports = mongoose.model('articles', articlesSchema);
